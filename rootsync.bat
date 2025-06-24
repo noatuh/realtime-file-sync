eca809081f5500dfe7c0094198f18d4ba964684b
noatuh <noatuh@outlook.com>
2025-06-23 21:09:57 -0400

    added rootsync

---
+++ rootsync.bat
@@ -0,0 +1,15 @@
+@echo off
+title C: DRIVE BACKUP
+:loop
+echo.
+echo ================================
+echo        C:\ BACKUP STARTING
+echo ================================
+
+robocopy C:\ "F:\Server PC\Root" /MIR /Z /W:1 /R:2 /XA:SH /A-:SH /XJ ^
+/XD "Windows" "$Recycle.Bin" "System Volume Information" "Users\Mark\AppData"
+
+echo.
+echo [INFO] Waiting 24 hours before next run...
+timeout /t 86400 >nul
+goto loop
