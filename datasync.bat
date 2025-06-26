cf491a1f44f45b8d9c31dbf0db4a43ab5e7a2cb4
noatuh <noatuh@outlook.com>
2025-06-25 22:18:32 -0400

    added allsync file

--- datasync.bat
+++ datasync.bat
@@ -1,1 +1,14 @@
-robocopy "D:\Data" "F:\Server PC\Server\Data" /MIR /Z /R:5 /W:5 /MON:1 /MOT:5
+@echo off
+title D: BACKUP
+:loop
+echo.
+echo ================================
+echo       D: BACKUP STARTING
+echo ================================
+
+robocopy "D:\Data" "F:\Server PC\Server\Data" /MIR /Z /R:5 /W:5
+
+echo.
+echo [INFO] Waiting 24 hours before next run...
+timeout /t 86400 >nul
+goto loop
